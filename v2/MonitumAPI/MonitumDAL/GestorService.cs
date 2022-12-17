129fb25c4f4101c3ea751b1c3176830e6a850148
duartemelo <43653646+duartemelo@users.noreply.github.com>
2022-12-17 17:57:08 +0000

    Documentation

--- v2/MonitumAPI/MonitumDAL/GestorService.cs
+++ v2/MonitumAPI/MonitumDAL/GestorService.cs
@@ -1,12 +1,20 @@
 ﻿using MonitumBOL.Models;
 using System.Data;
-using MonitumDAL.Utils;
 using System.Data.SqlClient;
 
 namespace MonitumDAL
 {
+    /// <summary>
+    /// Class que visa implementar todos os métodos de Data Access Layer referentes ao Gestor
+    /// Isto é, todos os acessos à base de dados relativos ao gestor estarão implementados em funções implementadas dentro desta classe
+    /// </summary>
     public class GestorService
     {
+        /// <summary>
+        /// Método que visa aceder à base de dados SQL Server e obter todos os registos de gestores lá criados (tabela Gestor)
+        /// </summary>
+        /// <param name="conString">String de conexão à base de dados, presente no projeto "MonitumAPI", no ficheiro appsettings.json</param>
+        /// <returns>Lista de gestores</returns>
         public static async Task<List<Gestor>> GetAllGestores(string conString)
         {
             var gestorList = new List<Gestor>();
