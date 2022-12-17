011191484a945639061a6143e782db20c633ae74
a10411 <93932321+a10411@users.noreply.github.com>
2022-12-17 18:09:55 +0000

    Models

---
+++ v2/MonitumAPI/MonitumBOL/Models/Horario_Sala.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitumBOL.Models
+{
+    internal class Horario_Sala
+    {
+    }
+}
