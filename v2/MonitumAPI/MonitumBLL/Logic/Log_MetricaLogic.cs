d64f9df55f9b94fbe456dfe9f05fc6239d23827a
a10411 <93932321+a10411@users.noreply.github.com>
2022-12-23 21:04:31 +0000

    Get All Log Metrica

---
+++ v2/MonitumAPI/MonitumBLL/Logic/Log_MetricaLogic.cs
@@ -0,0 +1,39 @@
+﻿using MonitumBLL.Utils;
+using MonitumBOL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitumBLL.Logic
+{
+    /// <summary>
+    /// Esta classe implementa todas as funções que, por sua vez, implementam a parte lógica de cada request relativo a logs de métricas
+    /// Nesta classe, abstraímo-nos de rotas, autorizações, links, etc. que dizem respeito à API
+    /// Porém, a API consome esta classe no sentido em que esta é responsável por transformar objetos vindos do DAL em responses.
+    /// Esta classe é a última a lidar com objetos (models) e visa abstrair a API dos mesmos
+    /// Gera uma response com um status code e dados
+    /// </summary>
+    public class Log_MetricaLogic
+    {
+        /// <summary>
+        /// Trata da parte lógica relativa à obtenção de todos as Logs de uma sala presentes na base de dados
+        /// </summary>
+        /// <param name="conString">Connection String da base de dados, que reside no appsettings.json do projeto MonitumAPI</param>
+        /// <returns>Response com Status Code, mensagem e dados (Logs recebidas do DAL)</returns>
+        public static async Task<Response> GetAllLogMetrica(string conString, int idSala)
+        {
+            Response response= new Response();
+            List<Log_Metrica> logList = await MonitumDAL.Log_MetricaService.GetAllLogMetrica(conString, idSala);
+            if (logList.Count != 0) 
+            {
+                response.StatusCode = StatusCodes.SUCCESS;
+                response.Message = "Sucesso na obtenção dos dados";
+                response.Data = logList;    
+            }
+            return response;
+        }
+        
+    }
+}
---
+++ v2/MonitumAPI/MonitumDAL/Log_MetricaService.cs
@@ -0,0 +1,43 @@
+﻿using MonitumBOL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitumDAL
+{
+    public class Log_MetricaService
+    {
+
+
+        /// <summary>
+        /// Método que visa aceder à base de dados SQL Server via query e obter as logs de métricas de uma sala
+        /// </summary>
+        /// <param name="conString">String de conexão à base de dados, presente no projeto "MonitumAPI", no ficheiro appsettings.json</param>
+        /// <param name="idSala">ID da Sala, da qual pretendemos obter as logs</param>
+        /// <returns>Logs da sala</returns>
+        public static async Task<List<Log_Metrica>> GetAllLogMetrica(string conString, int idSala)
+        {
+            var logList = new List<Log_Metrica>();  
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                SqlCommand cmd = new SqlCommand($"SELECT * FROM Logs_Metricas WHERE id_sala = {idSala}", con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Log_Metrica log = new Log_Metrica(rdr);
+                    logList.Add(log);
+                }
+                rdr.Close();
+                con.Close();
+            }
+            return logList;
+        }
+    }
+}
