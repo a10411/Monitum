5fec482224e66dd37a4b6eac89b6ca73bdca365b
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-01-04 23:03:46 +0000

    Front end more stuff

--- MonitumPDM/app/src/main/java/com/example/monitumpdm/UtilsAPI.kt
+++ MonitumPDM/app/src/main/java/com/example/monitumpdm/UtilsAPI.kt
@@ -2,6 +2,6 @@ package com.example.monitumpdm
 
 class UtilsAPI {
     fun connectionNgRok(): String {
-        return "https://3f24-2001-8a0-fe0e-4b00-11f7-20bb-27e1-bc4.eu.ngrok.io"
+        return "https://6cb7-2001-8a0-fe0e-4b00-11f7-20bb-27e1-bc4.eu.ngrok.io"
     }
 }
