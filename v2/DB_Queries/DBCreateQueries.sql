82a55ab4a2b59c26e4163847f9e89a16c42ae861
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-01-04 04:58:56 +0000

    CreateQueries Nome Sala

--- v2/DB_Queries/DBCreateQueries.sql
+++ v2/DB_Queries/DBCreateQueries.sql
@@ -4,7 +4,7 @@ CREATE TABLE Estabelecimento (id_estabelecimento int IDENTITY NOT NULL, nome var
 CREATE TABLE Horario_Sala (id_horario int IDENTITY NOT NULL, id_sala int NOT NULL, dia_semana varchar(255) NOT NULL, hora_entrada time(7) NOT NULL, hora_saida time(7) NOT NULL, PRIMARY KEY (id_horario));
 CREATE TABLE Log_Metrica (id_log int IDENTITY NOT NULL, id_sala int NOT NULL, id_metrica int NOT NULL, valor_metrica int NOT NULL, data_hora datetime NOT NULL, PRIMARY KEY (id_log));
 CREATE TABLE Metrica (id_metrica int IDENTITY NOT NULL, nome varchar(255) NOT NULL, medida varchar(255) NULL, PRIMARY KEY (id_metrica));
-CREATE TABLE Sala (id_sala int IDENTITY NOT NULL, id_estabelecimento int NOT NULL, id_estado int NOT NULL, PRIMARY KEY (id_sala));
+CREATE TABLE Sala (id_sala int IDENTITY NOT NULL, nome varchar(255) NOT NULL, id_estabelecimento int NOT NULL, id_estado int NOT NULL, PRIMARY KEY (id_sala));
 CREATE TABLE Estado (id_estado int IDENTITY NOT NULL, estado varchar(255) NOT NULL, PRIMARY KEY (id_estado));
 CREATE TABLE Comunicado (id_comunicado int IDENTITY NOT NULL, id_sala int NOT NULL, titulo varchar(255) NOT NULL, corpo varchar(255) NOT NULL, data_hora datetime NOT NULL, PRIMARY KEY (id_comunicado));
 CREATE TABLE Administrador (id_administrador int NOT NULL UNIQUE, email varchar(255) NOT NULL, password_hash varchar(255) NOT NULL, password_salt varchar(255) NOT NULL);
