3a9e091fede95839ea1d98c453291bd3e2d31aa3
Augustofsp <aafsp2000@gmail.com>
2023-01-05 00:14:42 +0000

    Update UtilsAPI.kt

--- MonitumPDM/app/src/main/java/com/example/monitumpdm/UtilsAPI.kt
+++ MonitumPDM/app/src/main/java/com/example/monitumpdm/UtilsAPI.kt
@@ -2,10 +2,6 @@ package com.example.monitumpdm
 
 class UtilsAPI {
     fun connectionNgRok(): String {
-<<<<<<< Updated upstream
-        return "https://6cb7-2001-8a0-fe0e-4b00-11f7-20bb-27e1-bc4.eu.ngrok.io"
-=======
         return "https://1eeb-89-153-206-195.eu.ngrok.io"
->>>>>>> Stashed changes
     }
 }
